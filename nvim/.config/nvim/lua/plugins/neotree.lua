3471ea0d3243b7241ddb6c6a534336eeaabac8a1
baradejoo <baradziejkamil@gmail.com>
2026-05-16 14:25:10 +0200

    fix neotree settings width

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -4,107 +4,117 @@
 -- ============================================================
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch  = "v3.x",
-  lazy    = false,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	lazy = false,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
 
-  keys = {
-    { "<leader>e",  "<cmd>Neotree toggle<CR>",               desc = "Toggle file tree" },
-    { "<leader>o",  "<cmd>Neotree focus<CR>",                desc = "Focus file tree" },
-    { "<leader>ge", "<cmd>Neotree git_status toggle<CR>",    desc = "Git status tree" },
-  },
+	keys = {
+		{ "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Toggle file tree" },
+		{ "<leader>o", "<cmd>Neotree focus<CR>", desc = "Focus file tree" },
+		{ "<leader>ge", "<cmd>Neotree git_status toggle<CR>", desc = "Git status tree" },
+	},
 
-  opts = {
-    close_if_last_window = true,
-    popup_border_style   = "rounded",
+	opts = {
+		close_if_last_window = true,
+		popup_border_style = "rounded",
 
-    default_component_configs = {
-      indent = {
-        indent_size         = 2,
-        padding             = 1,
-        with_markers        = true,
-        indent_marker       = "│",
-        last_indent_marker  = "└",
-        highlight           = "NeoTreeIndentMarker",
-        with_expanders      = true,
-        expander_collapsed  = "",
-        expander_expanded   = "",
-      },
-      icon = {
-        folder_closed = "",
-        folder_open   = "",
-        folder_empty  = "󰜌",
-      },
-      modified = { symbol = "●" },
-      git_status = {
-        symbols = {
-          added     = "✚",
-          modified  = "",
-          deleted   = "✖",
-          renamed   = "󰁕",
-          untracked = "",
-          ignored   = "",
-          unstaged  = "󰄱",
-          staged    = "",
-          conflict  = "",
-        },
-      },
-    },
+		default_component_configs = {
+			indent = {
+				indent_size = 2,
+				padding = 1,
+				with_markers = true,
+				indent_marker = "│",
+				last_indent_marker = "└",
+				highlight = "NeoTreeIndentMarker",
+				with_expanders = true,
+				expander_collapsed = "",
+				expander_expanded = "",
+			},
+			icon = {
+				folder_closed = "",
+				folder_open = "",
+				folder_empty = "󰜌",
+			},
+			modified = { symbol = "●" },
+			git_status = {
+				symbols = {
+					added = "✚",
+					modified = "",
+					deleted = "✖",
+					renamed = "󰁕",
+					untracked = "",
+					ignored = "",
+					unstaged = "󰄱",
+					staged = "",
+					conflict = "",
+				},
+			},
+		},
 
-    window = {
-      position = "left",
-      width    = 35,
-      mappings = {
-        ["<space>"] = "none",   -- nie konfliktuj z <leader>
-        ["l"]       = "open",
-        ["h"]       = "close_node",
-        ["H"]       = "toggle_hidden",
-        ["<CR>"]    = "open",
-        ["v"]       = "open_vsplit",
-        ["s"]       = "open_split",
-        ["t"]       = "open_tabnew",
-        ["R"]       = "refresh",
-        ["a"]       = "add",
-        ["d"]       = "delete",
-        ["r"]       = "rename",
-        ["c"]       = "copy",
-        ["m"]       = "move",
-        ["q"]       = "close_window",
-        ["?"]       = "show_help",
-      },
-    },
+		window = {
+			position = "left",
+			width = 25,
+			mappings = {
+				["<space>"] = "none", -- Do not conflict with <leader>
+				["<"] = function(state)
+					local new_width = math.max(10, vim.api.nvim_win_get_width(0) - 5)
+					state.window.width = new_width
+					vim.api.nvim_win_set_width(0, new_width)
+				end,
+				[">"] = function(state)
+					local new_width = vim.api.nvim_win_get_width(0) + 5
+					state.window.width = new_width
+					vim.api.nvim_win_set_width(0, new_width)
+				end,
+				["l"] = "open",
+				["h"] = "close_node",
+				["H"] = "toggle_hidden",
+				["<CR>"] = "open",
+				["v"] = "open_vsplit",
+				["s"] = "open_split",
+				["t"] = "open_tabnew",
+				["R"] = "refresh",
+				["a"] = "add",
+				["d"] = "delete",
+				["r"] = "rename",
+				["c"] = "copy",
+				["m"] = "move",
+				["q"] = "close_window",
+				["?"] = "show_help",
+			},
+		},
 
-    filesystem = {
-      -- Automatically reveal current file in tree
-      follow_current_file = {
-        enabled             = true,
-        leave_dirs_open     = false,
-      },
-      -- Use git to track modified files
-      use_libuv_file_watcher = true,
-      hijack_netrw_behavior  = "open_current",
-      filtered_items = {
-        visible        = false,
-        hide_dotfiles  = false,   -- show .env, .gitignore etc.
-        hide_gitignored = true,
-        hide_by_name   = {
-          "__pycache__",
-          ".git",
-          "node_modules",
-          ".pytest_cache",
-          ".mypy_cache",
-          ".ruff_cache",
-        },
-      },
-    },
+		filesystem = {
+			-- Automatically reveal current file in tree
+			follow_current_file = {
+				enabled = true,
+				leave_dirs_open = false,
+			},
+			-- Use git to track modified files
+			use_libuv_file_watcher = true,
+			hijack_netrw_behavior = "open_current",
+			filtered_items = {
+				visible = false,
+				hide_dotfiles = false, -- show .env, .gitignore etc.
+				hide_gitignored = true,
+				hide_by_name = {
+					"__pycache__",
+					".git",
+					"node_modules",
+					".pytest_cache",
+					".mypy_cache",
+					".ruff_cache",
+				},
+			},
+		},
 
-    git_status = {
-      window = { position = "float" },
-    },
-  },
+		git_status = {
+			window = { position = "float" },
+		},
+	},
 }
